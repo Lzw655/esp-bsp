8aa0f7a694b58b34c06320adefba5207de6d7a25
Liu Zhongwei <liuzhongwei@espressif.com>
2024-01-15 13:05:29 +0800

    test rgb & camera isr crash

--- examples/display_camera/main/display_camera.c
+++ examples/display_camera/main/display_camera.c
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: CC0-1.0
  */
@@ -18,13 +18,52 @@
 #include "esp_lcd_panel_ops.h"
 #include "esp_camera.h"
 
+#include "esp_lcd_panel_rgb.h"
+#include "esp_lcd_panel_ops.h"
+
+#define EXAMPLE_LCD_PIXEL_CLOCK_HZ     (18 * 1000 * 1000)
+#define EXAMPLE_LCD_BK_LIGHT_ON_LEVEL  1
+#define EXAMPLE_LCD_BK_LIGHT_OFF_LEVEL !EXAMPLE_LCD_BK_LIGHT_ON_LEVEL
+#define EXAMPLE_PIN_NUM_BK_LIGHT       4
+#define EXAMPLE_PIN_NUM_HSYNC          46
+#define EXAMPLE_PIN_NUM_VSYNC          3
+#define EXAMPLE_PIN_NUM_DE             0
+#define EXAMPLE_PIN_NUM_PCLK           9
+#define EXAMPLE_PIN_NUM_DATA0          14 // B0
+#define EXAMPLE_PIN_NUM_DATA1          13 // B1
+#define EXAMPLE_PIN_NUM_DATA2          12 // B2
+#define EXAMPLE_PIN_NUM_DATA3          11 // B3
+#define EXAMPLE_PIN_NUM_DATA4          10 // B4
+#define EXAMPLE_PIN_NUM_DATA5          39 // G0
+#define EXAMPLE_PIN_NUM_DATA6          38 // G1
+#define EXAMPLE_PIN_NUM_DATA7          45 // G2
+#define EXAMPLE_PIN_NUM_DATA8          48 // G3
+#define EXAMPLE_PIN_NUM_DATA9          47 // G4
+#define EXAMPLE_PIN_NUM_DATA10         21 // G5
+#define EXAMPLE_PIN_NUM_DATA11         1  // R0
+#define EXAMPLE_PIN_NUM_DATA12         2  // R1
+#define EXAMPLE_PIN_NUM_DATA13         42 // R2
+#define EXAMPLE_PIN_NUM_DATA14         41 // R3
+#define EXAMPLE_PIN_NUM_DATA15         40 // R4
+#define EXAMPLE_PIN_NUM_DISP_EN        -1
+
+// The pixel number in horizontal and vertical
+#define EXAMPLE_LCD_H_RES              800
+#define EXAMPLE_LCD_V_RES              480
+
+#if CONFIG_EXAMPLE_DOUBLE_FB
+#define EXAMPLE_LCD_NUM_FB             2
+#else
+#define EXAMPLE_LCD_NUM_FB             1
+#endif // CONFIG_EXAMPLE_DOUBLE_FB
+
 static const char *TAG = "example";
 
 void app_main(void)
 {
     bsp_i2c_init();
-    bsp_display_start();
-    bsp_display_backlight_on(); // Set display brightness to 100%
+    // bsp_display_start();
+    // bsp_display_backlight_on(); // Set display brightness to 100%
 
     // Initialize the camera
     const camera_config_t camera_config = BSP_CAMERA_DEFAULT_CONFIG;
@@ -38,19 +77,73 @@ void app_main(void)
     ESP_LOGI(TAG, "Camera Init done");
 
     // Create LVGL canvas for camera image
-    bsp_display_lock(0);
-    lv_obj_t *camera_canvas = lv_canvas_create(lv_scr_act());
-    assert(camera_canvas);
-    lv_obj_center(camera_canvas);
-    bsp_display_unlock();
+    // bsp_display_lock(0);
+    // lv_obj_t *camera_canvas = lv_canvas_create(lv_scr_act());
+    // assert(camera_canvas);
+    // lv_obj_center(camera_canvas);
+    // bsp_display_unlock();
+
+    ESP_LOGI(TAG, "Install RGB LCD panel driver");
+    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_rgb_panel_config_t panel_config = {
+        .data_width = 16, // RGB565 in parallel mode, thus 16bit in width
+        .psram_trans_align = 64,
+        .num_fbs = EXAMPLE_LCD_NUM_FB,
+#if CONFIG_EXAMPLE_USE_BOUNCE_BUFFER
+        .bounce_buffer_size_px = 10 * EXAMPLE_LCD_H_RES,
+#endif
+        .clk_src = LCD_CLK_SRC_DEFAULT,
+        .disp_gpio_num = EXAMPLE_PIN_NUM_DISP_EN,
+        .pclk_gpio_num = EXAMPLE_PIN_NUM_PCLK,
+        .vsync_gpio_num = EXAMPLE_PIN_NUM_VSYNC,
+        .hsync_gpio_num = EXAMPLE_PIN_NUM_HSYNC,
+        .de_gpio_num = EXAMPLE_PIN_NUM_DE,
+        .data_gpio_nums = {
+            EXAMPLE_PIN_NUM_DATA0,
+            EXAMPLE_PIN_NUM_DATA1,
+            EXAMPLE_PIN_NUM_DATA2,
+            EXAMPLE_PIN_NUM_DATA3,
+            EXAMPLE_PIN_NUM_DATA4,
+            EXAMPLE_PIN_NUM_DATA5,
+            EXAMPLE_PIN_NUM_DATA6,
+            EXAMPLE_PIN_NUM_DATA7,
+            EXAMPLE_PIN_NUM_DATA8,
+            EXAMPLE_PIN_NUM_DATA9,
+            EXAMPLE_PIN_NUM_DATA10,
+            EXAMPLE_PIN_NUM_DATA11,
+            EXAMPLE_PIN_NUM_DATA12,
+            EXAMPLE_PIN_NUM_DATA13,
+            EXAMPLE_PIN_NUM_DATA14,
+            EXAMPLE_PIN_NUM_DATA15,
+        },
+        .timings = {
+            .pclk_hz = EXAMPLE_LCD_PIXEL_CLOCK_HZ,
+            .h_res = EXAMPLE_LCD_H_RES,
+            .v_res = EXAMPLE_LCD_V_RES,
+            // The following parameters should refer to LCD spec
+            .hsync_back_porch = 40,
+            .hsync_front_porch = 20,
+            .hsync_pulse_width = 1,
+            .vsync_back_porch = 8,
+            .vsync_front_porch = 4,
+            .vsync_pulse_width = 1,
+            .flags.pclk_active_neg = true,
+        },
+        .flags.fb_in_psram = true, // allocate frame buffer in PSRAM
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_rgb_panel(&panel_config, &panel_handle));
+
+    ESP_LOGI(TAG, "Initialize RGB LCD panel");
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
 
     camera_fb_t *pic;
     while (1) {
         pic = esp_camera_fb_get();
         if (pic) {
-            bsp_display_lock(0);
-            lv_canvas_set_buffer(camera_canvas, pic->buf, pic->width, pic->height, LV_IMG_CF_TRUE_COLOR);
-            bsp_display_unlock();
+            // bsp_display_lock(0);
+            // lv_canvas_set_buffer(camera_canvas, pic->buf, pic->width, pic->height, LV_IMG_CF_TRUE_COLOR);
+            // bsp_display_unlock();
             esp_camera_fb_return(pic);
         } else {
             ESP_LOGE(TAG, "Get frame failed");
